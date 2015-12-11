7285c17376d579a7b2892d1dc947f7fe4d961e6a
Ranjeet Negi <rnjet.negi@gmail.com>
2015-12-12 00:07:57 +0530

    same smile copy added to right

--- js/Script.js
+++ js/Script.js
@@ -1,12 +1,8 @@
 var numberOfFaces = 5;
 var theleftSide = document.getElementById("leftSide");
+var therightSide = document.getElementById("rightSide");
 function generateFaces(){
-	
-	
-
-	
 	for(var i= 0; i<numberOfFaces; i++){
-		var leftSide = document.getElementById("leftSide");
 		var PositionLeft =  Math.random()*400;
 		var PositionTop = Math.random()*400;
 		var face = document.createElement("img");
@@ -14,6 +10,9 @@ function generateFaces(){
 		face.classList.add("image");
 		face.style.top = PositionTop + "px";
 		face.style.left = PositionLeft + "px";
-		leftSide.appendChild(face);
+		theleftSide.appendChild(face);
+		rface = face.cloneNode(true);
+		therightSide.appendChild(rface);
+
 	}
 }
