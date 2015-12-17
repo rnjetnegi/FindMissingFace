81eaf02195b3575307f45ad70799e11191fa6880
Ranjeet Negi <rnjet.negi@gmail.com>
2015-12-18 01:07:21 +0530

    bootstrap files added

--- js/Script.js
+++ js/Script.js
@@ -1,6 +1,8 @@
 var numberOfFaces = 5;
 var theleftSide = document.getElementById("leftSide");
 var therightSide = document.getElementById("rightSide");
+var theBody = document.getElementsByTagName("body")[0];
+
 function generateFaces(){
 	for(var i= 0; i<numberOfFaces; i++){
 		var PositionLeft =  Math.random()*400;
@@ -12,16 +14,25 @@ function generateFaces(){
 		face.style.left = PositionLeft + "px";
 		theleftSide.appendChild(face);
 	}
+	console.log("***********************after for loop****************************");
 	therightSide.appendChild(theleftSide.cloneNode(true));
 	var last_child = therightSide.firstChild.lastChild;
 	therightSide.childNodes[0].removeChild(last_child);
 
+	console.log("***********************Before click function happens*******************************")
+	theBody.lastChild.onClick = function nextLevel(event){
+	event.stopPropagation();
+	numberOfFaces += 1;
+	generateFaces();
+	console.log("Inside nextLevel");
+};
 
-	var theBody = document.getElementsByTagName("body")[0];
-	theBody.lastChild.onClick = nextLevel(event){
-		 event.stopPropagation();
-		 numberOfFaces += 1;
-		 generateFaces();
-	}
-
+theBody.onClick = function gameOver() {
+	alert("Game Over!");
+	theBody.onclick = null;
+	theLeftSide.lastChild.onclick = null;
+	console.log("Inside gameOver");
+};
 }
+
+
